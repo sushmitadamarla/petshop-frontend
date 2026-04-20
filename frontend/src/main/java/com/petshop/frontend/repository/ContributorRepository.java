d886dfd24ed23dddb5b0925b082ab01ea1c35fe0
sushmitadamarla <158757381+sushmitadamarla@users.noreply.github.com>
2026-04-20 20:32:54 +0530

    update ContributorRepository interface

--- frontend/src/main/java/com/petshop/frontend/repository/ContributorRepository.java
+++ frontend/src/main/java/com/petshop/frontend/repository/ContributorRepository.java
@@ -1,11 +1,1 @@
-package com.petshop.frontend.repository;
 
-import com.petshop.frontend.model.Contributor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
-
-@Repository
-public interface ContributorRepository extends JpaRepository<Contributor, Long> {
-    Optional<Contributor> findByUsername(String username);
-}
